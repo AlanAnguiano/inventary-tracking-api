a8224d8a9c95f6acd345a844a6c32ca1a69a5078
Ozmar Ugarte <ozmar_11@hotmail.com>
2020-06-16 23:19:19 -0500

    Override assign_provider_attrs method
    - Override assign_provider_attrs method from devise token auth
    to save the required params from the auth provider

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '*path', to: 'angular#index'
+
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    omniauth_callbacks: 'overrides/omniauth_callbacks'
+  }
 end
