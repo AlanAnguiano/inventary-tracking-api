1eebc37be2f818a0a32fcf09f508ad1d6097ea77
Ozmar Ugarte <ozmar_11@hotmail.com>
2020-07-04 15:57:19 -0500

    Load the ng token config in the app module

--- inventory-tracker/src/app/app.module.ts
+++ inventory-tracker/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { GoogleOauthConfigModule } from './google-oauth-config/google-oauth-config.module';
+import { HttpClientModule } from '@angular/common/http';
+import { OauthCallbackComponentComponent } from './oauth-callback-component/oauth-callback-component.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    OauthCallbackComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    GoogleOauthConfigModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
