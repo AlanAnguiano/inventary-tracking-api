3b67b9d7dcd7ecd7defbc7a2562ad90c54788ecd
Ozmar Ugarte <ozmar_11@hotmail.com>
2020-07-26 16:45:51 -0500

    Update test
    - Update test to the new UI

--- inventory-tracker/e2e/src/app.po.ts
+++ inventory-tracker/e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('div.top-bar')).getText() as Promise<string>;
   }
 }
