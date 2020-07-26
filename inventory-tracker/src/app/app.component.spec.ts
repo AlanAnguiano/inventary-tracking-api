b2ffb2b41cf71823ea743788a55e107a6ddaa1c7
Ozmar Ugarte <ozmar_11@hotmail.com>
2020-07-26 16:23:55 -0500

    Add top bar and render component title
    - Add the top bar and render current component title there so
    the user knows where is

--- inventory-tracker/src/app/app.component.spec.ts
+++ inventory-tracker/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('inventory-tracker app is running!');
+    expect(compiled.querySelector('div.top-bar').textContent).toContain('inventory-tracker');
   });
 });
