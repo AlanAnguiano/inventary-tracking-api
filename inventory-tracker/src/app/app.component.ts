aa9a4186c41b66705709c4c9ff9795268db948d7
Ozmar Ugarte <ozmar_11@hotmail.com>
2020-08-06 13:32:08 -0500

    Add styles and user data to the layout
    - Render user profile image
    - Render user name
    - Render sign out link
    - Fix broken tests importing required dependencies

--- inventory-tracker/src/app/app.component.ts
+++ inventory-tracker/src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularTokenService } from 'angular-token';
+import {UtilsService} from './utils/utils.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'inventory-tracker';
+  user = {
+    image: '',
+    name: '',
+  };
+
+  constructor(public tokenService: AngularTokenService, private utilsService: UtilsService) { }
+
+  ngOnInit(): void {
+    if (!this.tokenService.userSignedIn()) { return; }
+
+    this.tokenService.validateToken().subscribe(
+      userData => this.setUserData(userData),
+      error => this.utilsService.logError(error, 'This error has occurred when fetching user data'),
+    );
+  }
 
   onRouterOutletActivate(event: any): void {
     this.title = event.title;
+  }
+
+  setUserData = (userData) => {
+    this.user = userData.data;
   }
 }
