43602052d6f7b927fd0b57fdabbffa3259cb68b0
Ozmar Ugarte <ozmar_11@hotmail.com>
2020-08-06 19:12:03 -0500

    Add APP_INITIALIZER function
    - Add the APP_INITIALIZER function to be loaded before Angular
    to know if the user is logged in

--- inventory-tracker/src/app/app.module.ts
+++ inventory-tracker/src/app/app.module.ts
@@ -1,19 +1,29 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppService } from './app.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { GoogleOauthConfigModule } from './google-oauth-config/google-oauth-config.module';
-import { HttpClientModule } from '@angular/common/http';
+import { LoggedInGuardGuard } from './guards/logged-in-guard.guard';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OauthCallbackComponentComponent } from './oauth-callback-component/oauth-callback-component.component';
 import { SigninComponent } from './signin/signin.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SignoutComponent } from './signout/signout.component';
+
+function app_init(appService: AppService) {
+  return () => appService.initializeApp();
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
-    OauthCallbackComponentComponent
+    OauthCallbackComponentComponent,
+    DashboardComponent,
+    SignoutComponent
   ],
   imports: [
     AppRoutingModule,
@@ -22,7 +32,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     HttpClientModule,
     NgbModule
   ],
-  providers: [],
+  providers: [
+    AppService,
+    { provide: APP_INITIALIZER, useFactory: app_init, deps: [AppService], multi: true },
+    LoggedInGuardGuard,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
